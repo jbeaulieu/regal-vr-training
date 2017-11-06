0f673eb696b4385e84fdd48a428841ca9683e1c0
alitarraf <ali.tarraf@gmail.com>
2017-11-06 17:03:13 -0600

    PDF viewer and button...3d pdf not loading yet

--- RegalTraining/app/src/main/java/com/regal/training/MainActivity.java
+++ RegalTraining/app/src/main/java/com/regal/training/MainActivity.java
@@ -3,11 +3,18 @@ package com.regal.training;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class MainActivity extends AppCompatActivity {
+    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pdfView = (PDFView)findViewById(R.id.pdfView);
+        //This function reads PDF from Assets folder
+        pdfView.fromAsset("615405.pdf").load();
     }
 }
