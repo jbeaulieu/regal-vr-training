5082c296efda0e2510a3da52c6625ce446506f06
Jon Beaulieu <jon@jonbeaulieu.com>
2017-11-29 12:14:09 -0600

    Nav drawer customization
    Added appropriate branding, colors, and menu options to navigation drawer

--- RegalTraining/app/src/main/java/com/regal/training/MainActivity.java
+++ RegalTraining/app/src/main/java/com/regal/training/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_3dview) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_assembly) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_bom) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_game) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_feedback) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
